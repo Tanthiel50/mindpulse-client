b91c6692cd067e6e4e8aea8bf033084a187618c0
Tanthiel50 <66637911+Tanthiel50@users.noreply.github.com>
2024-03-04 11:56:45 +0100

    theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,6 +1,18 @@
-import { createTheme } from '@mui/material/styles';
+// import { createTheme } from '@mui/material/styles';
 
-const theme = createTheme({
+// const theme = createTheme({
+//   palette: {
+//     black: '#000',
+//     white: '#fff',
+//     purplish: '#AE8ABE',
+//     bluish: '#8390C8',
+//     background: '#121212',
+//   },
+// });
+
+// export default theme;
+
+export const theme= {
   palette: {
     black: '#000',
     white: '#fff',
@@ -8,6 +20,9 @@ const theme = createTheme({
     bluish: '#8390C8',
     background: '#121212',
   },
-});
-
-export default theme;
+  button :{
+    backgroundColor: 'rgba(174, 138, 190, 1)',
+    color: 'white',
+    padding: '16px 32px',
+  }
+};
