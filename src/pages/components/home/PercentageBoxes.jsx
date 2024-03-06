9d9bbb827805262fbefa92052ef89080b1ae3023
Tanthiel50 <66637911+Tanthiel50@users.noreply.github.com>
2024-03-06 16:56:30 +0100

    padding percentage box

--- src/pages/components/home/PercentageBoxes.jsx
+++ src/pages/components/home/PercentageBoxes.jsx
@@ -4,7 +4,7 @@ import { Paper, Typography, Grid } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 const StyledPaper = styled(Paper)(({ theme }) => ({
-  padding: theme.spacing(4),
+  padding: theme.spacing(2),
   textAlign: 'center',
   height: '100%',
   borderRadius: 0,
@@ -14,7 +14,14 @@ const PercentageBoxes = ({ percentage, description, backgroundColor, color }) =>
   return (
     <Grid item xs={12} sm={4}>
       <StyledPaper sx={{ backgroundColor, color }}>
-        <Typography variant="h3" sx={{ fontWeight: 'bold' }}>
+        <Typography 
+        variant="h3" 
+        sx={{ 
+          fontWeight: 'bold',
+          paddingBottom: 4,
+          paddingTop: 2, 
+          }}
+          >
           {percentage}
         </Typography>
         <Typography variant="body1">
