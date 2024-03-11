590b883b360abf8d6424b1f05f510790bd404959
Tanthiel50 <66637911+Tanthiel50@users.noreply.github.com>
2024-03-11 10:51:37 +0100

    view and delete message

--- src/pages/admin/messages/MessagePage.jsx
+++ src/pages/admin/messages/MessagePage.jsx
@@ -1,52 +1,92 @@
-import Box from "@mui/material/Box";
+import React, { useEffect, useState } from "react";
+import { Box, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { axiosInstance } from "../../../http-common/axios-configuration";
+import DeleteIcon from "@mui/icons-material/Delete";
+import VisibilityIcon from "@mui/icons-material/Visibility"; // Importer l'icône d'oeil
+import { toast } from "react-toastify";
 import Sidebar from "../../components/admin/Sidebar";
-import TableView from "../../components/admin/TableView";
-
 
 function MessagePage() {
+  const [data, setData] = useState([]);
+  const navigate = useNavigate();
 
-    const userFriendlyHeaders = {
-      senderFirstName: "Nom",
-      senderLastName: "Prénom",
-      senderEmail: "Email",
-      senderCompany: "Société",
-      senderPhone: "Téléphone",
-      subject: "Sujet",
-      title: "Title",
-      message: "Message",
-      created_at: "Date d'envoi",
-      actions: "Actions"
+  const userFriendlyHeaders = {
+    senderFirstName: "Nom",
+    senderLastName: "Prénom",
+    senderEmail: "Email",
+    senderCompany: "Soiété",
+    senderPhone: "Téléphone",
+    subject: "Sujet",
+    title: "Title",
+    message: "Message",
+    created_at: "Date d'envoi",
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axiosInstance.get("/messages");
+        setData(response.data);
+      } catch (error) {
+        toast.error("Erreur lors du chargement des messages");
+        console.error("Error fetching data:", error);
+      }
     };
+
+    fetchData();
+  }, []);
+
+  const handleDelete = async (id) => {
+    try {
+      await axiosInstance.delete(`/messages/${id}`);
+      toast.success('Message supprimé avec succès');
+      setData(data.filter(item => item.id !== id));
+    } catch (error) {
+      toast.error('Erreur lors de la suppression');
+      console.error('Erreur lors de la suppression:', error);
+    }
+  };
 
   return (
     <Box sx={{ display: "flex", height: "100vh" }}>
       <Sidebar />
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <TableView
-          title="Messages"
-          createPath=""
-          tableHeaders={[
-            "senderFirstName",
-            "senderLastName",
-            "senderEmail",
-            "senderCompany",
-            "senderPhone",
-            "subject",
-            "title",
-            "message",
-            "created_at",
-            "actions",
-          ]}
-          fetchUrl="/messages"
-          userFriendlyHeaders={userFriendlyHeaders}
-          actionButtonConfig={{
-            editPath: "/admin/message/:id",
-            deletePath: "/admin/categories/delete/:id",
-          }}
-        />
+      <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop: "5rem" }}>
+        <Typography variant="h2" sx={{ fontWeight: "bold", color: "white" }}>
+          Messages
+        </Typography>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                {Object.values(userFriendlyHeaders).map((header, index) => (
+                  <TableCell key={index}>{header}</TableCell>
+                ))}
+                <TableCell>Actions</TableCell> 
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.map((row) => (
+                <TableRow key={row.id}>
+                  {Object.keys(userFriendlyHeaders).map((field) => (
+                    <TableCell key={`${row.id}-${field}`}>{row[field]}</TableCell>
+                  ))}
+                  <TableCell>
+                    <Button onClick={() => navigate(`/admin/message/${row.id}`)}>
+                      <VisibilityIcon sx={{ color: "black" }} />
+                    </Button>
+                    <Button onClick={() => handleDelete(row.id)}>
+                      <DeleteIcon sx={{ color: "red" }} />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Box>
     </Box>
-  )
+  );
 }
 
-export default MessagePage
+export default MessagePage;
--- src/pages/admin/messages/ViewMessage.jsx
+++ src/pages/admin/messages/ViewMessage.jsx
@@ -1,53 +1,94 @@
-import React from "react";
-import Box from "@mui/material/Box";
+import React, { useEffect, useState } from "react";
+import { Box, Typography, CircularProgress, Paper } from "@mui/material";
+import { useParams } from "react-router-dom";
+import { axiosInstance } from "../../../http-common/axios-configuration";
+import { toast } from "react-toastify";
 import Sidebar from "../../components/admin/Sidebar";
-import TableView from "../../components/admin/TableView";
-
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
 
 function ViewMessage() {
+  const { id } = useParams();
+  const [message, setMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
 
-    const userFriendlyHeaders = {
-      senderFirstName: "Nom",
-      senderLastName: "Prénom",
-      senderEmail: "Email",
-      senderCompany: "Soiété",
-      senderPhone: "Téléphone",
-      subject: "Sujet",
-      title: "Title",
-      message: "Message",
-      created_at: "Date d'envoi",
-      actions: "Actions"
+  useEffect(() => {
+    const fetchMessage = async () => {
+      try {
+        const response = await axiosInstance.get(`/messages/${id}`);
+        setMessage(response.data);
+      } catch (error) {
+        toast.error("Erreur lors du chargement du message");
+        console.error("Error fetching message:", error);
+      } finally {
+        setIsLoading(false);
+      }
     };
+
+    fetchMessage();
+  }, [id]);
+
+  const handleBack = () => {
+    navigate(-1); 
+  };
 
   return (
     <Box sx={{ display: "flex", height: "100vh" }}>
       <Sidebar />
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <TableView
-          title="Categories"
-          createPath="/admin/create-categorie"
-          tableHeaders={[
-            "senderFirstName",
-            "senderLastName",
-            "senderEmail",
-            "senderCompany",
-            "senderPhone",
-            "subject",
-            "title",
-            "message",
-            "created_at",
-            "actions",
-          ]}
-          fetchUrl="/messages"
-          userFriendlyHeaders={userFriendlyHeaders}
-          actionButtonConfig={{
-            editPath: "/admin/edit-categorie/:id",
-            deletePath: "/admin/categories/delete/:id",
+      <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop: "5rem" }}>
+        {isLoading ? (
+          <CircularProgress />
+        ) : (
+          <>
+          <Paper elevation={3} sx={{ p: 3 }}>
+            <Typography variant="h4" gutterBottom>
+              Détails du Message
+            </Typography>
+            <Typography variant="body1" sx={{ mt: 2 }}>
+              <strong>Nom :</strong> {message?.senderFirstName} {message?.senderLastName}
+            </Typography>
+            <Typography variant="body1" sx={{ mt: 2 }}>
+              <strong>Email :</strong> {message?.senderEmail}
+            </Typography>
+            <Typography variant="body1" sx={{ mt: 2 }}>
+              <strong>Société :</strong> {message?.senderCompany}
+            </Typography>
+            <Typography variant="body1" sx={{ mt: 2 }}>
+              <strong>Téléphone :</strong> {message?.senderPhone}
+            </Typography>
+            <Typography variant="body1" sx={{ mt: 2 }}>
+              <strong>Sujet :</strong> {message?.subject}
+            </Typography>
+            <Typography variant="body1" sx={{ mt: 2 }}>
+              <strong>Message :</strong> {message?.message}
+            </Typography>
+            <Typography variant="body1" sx={{ mt: 2 }}>
+              <strong>Date d'envoi :</strong> {new Date(message?.created_at).toLocaleDateString()}
+            </Typography>
+          </Paper>
+           <Button
+           startIcon={<ArrowBackIcon />}
+           onClick={handleBack}
+           sx={{
+            marginTop: "1rem",
+            backgroundColor: "white",
+            color: "black",
+            fontWeight: "bold",
+            "&:hover": {
+              backgroundColor: "black",
+              color: "white",
+            },
           }}
-        />
+         >
+           Retour
+         </Button>
+         </>
+        )}
       </Box>
     </Box>
-  )
+  );
 }
 
-export default ViewMessage
+export default ViewMessage;
