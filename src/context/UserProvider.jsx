8444d2910c076625aff1b0d3228d92603e6ee324
Tanthiel50 <66637911+Tanthiel50@users.noreply.github.com>
2024-03-20 14:38:05 +0100

    hash role

--- src/context/UserProvider.jsx
+++ src/context/UserProvider.jsx
@@ -6,17 +6,27 @@ const UserContext = createContext();
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
+
+  const generateHash = (value) => {
+    return value.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0).toString();
+  }
+
   useEffect(() => {
     const fetchUser = async () => {
       try {
         const token = localStorage.getItem('token');
         const role = localStorage.getItem('role');
+        const roleHash = localStorage.getItem('roleHash');
 
         if (token) {
           axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
           const response = await axiosInstance.get('/me');
           setUser({ ...response.data, role });
+        }
+        if (generateHash(role) !== roleHash) {
+          // Les données semblent avoir été manipulées
+          logout();
         }
       } catch (error) {
         console.error('Error fetching user:', error);
@@ -26,11 +36,37 @@ export const UserProvider = ({ children }) => {
     fetchUser();
   }, []);
 
+  useEffect(() => {
+    const verifyIntegrity = () => {
+      const token = localStorage.getItem('token');
+      const role = localStorage.getItem('role');
+      const roleHash = localStorage.getItem('roleHash');
+  
+      if (token && role) {
+        if (generateHash(role) !== roleHash) {
+          // Si le hash ne correspond pas, cela indique une possible manipulation
+          logout();
+        }
+      }
+    };
+  
+    // Exécutez immédiatement une fois au démarrage
+    verifyIntegrity();
+  
+    // Configurez ensuite une vérification périodique toutes les 2 minutes
+    const intervalId = setInterval(verifyIntegrity, 120000); // 120000 ms = 2 minutes
+  
+    // N'oubliez pas de nettoyer l'intervalle lorsque le composant est démonté
+    // pour éviter des effets indésirables ou des fuites de mémoire
+    return () => clearInterval(intervalId);
+  }, []);
+
   const login = async (email, password) => {
     try {
       const response = await axiosInstance.post('/login', { email, password });
       localStorage.setItem('token', response.data.token);
       localStorage.setItem('role', response.data.role);
+      localStorage.setItem('roleHash', generateHash(response.data.role));
       setUser({ ...response.data.user, role: response.data.role });
       return response;
     } catch (error) {
@@ -42,6 +78,7 @@ export const UserProvider = ({ children }) => {
   const logout = (onLoggedOut) => {
     localStorage.removeItem('token');
     localStorage.removeItem('role');
+    localStorage.removeItem('roleHash');
     setUser(null);
     if (onLoggedOut) {
         onLoggedOut();
